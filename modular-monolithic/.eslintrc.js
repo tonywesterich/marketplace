3f22e047a4d9bf47bca0be8af11a8feb5bcadfce
Tony Westerich <tonywesterich@gmail.com>
2024-04-02 16:37:11 -0300

    Adds lint rule to enforce the usage of @app instead of relative imports
    Adds module-alias and configure it.

--- modular-monolithic/.eslintrc.js
+++ modular-monolithic/.eslintrc.js
@@ -5,7 +5,10 @@ module.exports = {
     tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: [
+    '@typescript-eslint/eslint-plugin',
+    'no-relative-import-paths'
+  ],
   extends: [
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
@@ -21,5 +24,13 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    'no-relative-import-paths/no-relative-import-paths': [
+      'warn',
+      {
+        allowSameFolder: true,
+        prefix: '@app',
+        rootDir: 'src'
+      }
+    ],
   },
 };
