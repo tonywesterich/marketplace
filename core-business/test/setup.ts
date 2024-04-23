da8825daefe9f216b1d8f835fbf0a1c3401162c4
Tony Westerich <tonywesterich@gmail.com>
2024-04-23 15:15:09 -0300

    Fix tests setup

--- core-business/test/setup.ts
+++ core-business/test/setup.ts
@@ -1,12 +1,16 @@
 import dotenv from 'dotenv';
 import fs from 'fs';
 
+const ENV_FILE = '.env';
 const TEST_ENV_FILE = '.env.test';
 
-dotenv.config({ path: TEST_ENV_FILE });
+if (!fs.existsSync(ENV_FILE)) {
+  throw new Error('.env file not found');
+}
 
 if (!fs.existsSync(TEST_ENV_FILE)) {
   throw new Error('.env.test file not found');
 }
 
-dotenv.config({ path: TEST_ENV_FILE });
+dotenv.config({ path: ENV_FILE });
+dotenv.config({ path: TEST_ENV_FILE, override: true });
